d3ac19e086935a8dc5c2e38ccb152007cad535f9
nymda <puffleboy101@hotmail.co.uk>
2020-05-17 22:11:03 +0100

    commit

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TerrariaEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        lib lib = new lib();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Input File";
+                dlg.Filter = "Player | *.plr";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    lib.getRawData(dlg.FileName);              
+                }
+            }
+        }
+    }
+}
